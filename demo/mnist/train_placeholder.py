aa5319549ab2ae13b5f4c598448ed25f2c20389d
baiyang <bby1220@163.com>
2021-05-05 18:54:17 +0800

    Succeed to load mnist data

--- demo/mnist/train_placeholder.py
+++ demo/mnist/train_placeholder.py
@@ -1,13 +1,24 @@
 # -*- coding: utf-8 -*-
 import tensorflow as tf
+from datasets import load_mnist
+
+DATA_FOLDER = r'.\data'
 
 
-def load_data():
-    pass
+def load_data(folder):
+    data = load_mnist(folder)
+    train_data = data.get('train_data')
+    train_labels = data.get('train_labels')
+    test_data = data.get('test_data')
+    test_labels = data.get('test_labels')
+    return train_data, train_labels, test_data, test_labels
 
 
 def train():
     # load data
+    train_data, train_labels, test_data, test_labels = load_data(DATA_FOLDER)
+    print(train_data.shape, train_labels.shape,
+          test_data.shape, test_labels.shape)
 
     # train
 
@@ -15,3 +26,7 @@ def train():
 
     # save model
     pass
+
+
+if __name__ == '__main__':
+    train()
