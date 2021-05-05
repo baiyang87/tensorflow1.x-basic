ddcc9dcdc840844816f41a941d97a23399feac59
baiyang <bby1220@163.com>
2021-05-05 09:40:34 +0800

    Add softmax_cross_entropy and sigmoid_cross_entropy

--- losses/classification.py
+++ losses/classification.py
@@ -1,1 +1,36 @@
 # -*- coding: utf-8 -*-
+import tensorflow as tf
+
+
+def softmax_cross_entropy(onehot_labels, logits):
+    """
+    Cross entropy computed by softmax logits.
+
+    Parameters
+    ----------
+    onehot_labels: One-hot-encoded labels
+    logits: Logits outputs of the network.
+    """
+    onehot_labels = tf.cast(onehot_labels, logits.dtype)
+    probabilities = tf.nn.softmax(logits)
+    loss = tf.reduce_mean(tf.reduce_sum(
+        -onehot_labels * tf.log(probabilities), axis=-1))
+    return loss
+
+
+def sigmoid_cross_entropy(labels, logits):
+    """
+    Cross entropy computed by sigmoid logits.
+
+    Parameters
+    ----------
+    labels: `[batch_size, num_classes]` target integer labels in `{0, 1}`
+    logits: Float `[batch_size, num_classes]` logits outputs of the network
+    """
+    labels = tf.cast(labels, logits.dtype)
+    zeros = tf.zeros_like(logits, dtype=logits.dtype)
+    condition = tf.greater(logits, zeros)
+    relu_logits = tf.where(condition, logits, zeros)
+    loss = tf.reduce_mean(relu_logits - logits * labels +
+                          tf.log1p(tf.exp(-tf.abs(logits))))
+    return loss
