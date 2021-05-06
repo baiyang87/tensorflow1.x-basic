3988ece6a13623ff2243a7d398a22f5082f30ac0
baiyang <bby1220@163.com>
2021-05-06 22:52:33 +0800

    Modify __init__.py for losses

--- losses/__init__.py
+++ losses/__init__.py
@@ -1,1 +1,8 @@
 # -*- coding: utf-8 -*-
+from datasets.mnist import load as load_mnist
+from losses.classification import softmax_cross_entropy
+from losses.classification import sigmoid_cross_entropy
+
+from losses.regression import mean_absolute_error
+from losses.regression import mean_squared_error
+from losses.regression import huber_loss
