0e5a1a0a98954df280f64a1fc89c9c45ecfe70c1
baiyang <bby1220@163.com>
2021-05-08 00:14:18 +0800

    Complete train and test process

--- demo/mnist/train_placeholder.py
+++ demo/mnist/train_placeholder.py
@@ -6,7 +6,9 @@ from datasets import load_mnist
 from datasets import make_one_hot_labels
 from classification_net.plain_net import PlainNet
 from losses import softmax_cross_entropy
+from metrics import logits_accuracy
 
+# parameters
 DATA_FOLDER = r'.\data'
 NUM_CLASSES = 10
 EPOCH = 10
@@ -41,11 +43,28 @@ def make_model():
     return model
 
 
+def random_shuffle(data, labels):
+    number = data.shape[0]
+    random_index = np.arange(number)
+    np.random.shuffle(random_index)
+    data = data[random_index]
+    labels = labels[random_index]
+    return data, labels
+
+
+def load_batch(data, labels, index_beg, batch_size):
+    total_num = data.shape[0]
+    index_end = min(index_beg + batch_size, total_num)
+    batch_data = data[index_beg:index_end]
+    batch_labels = labels[index_beg:index_end]
+    return batch_data, batch_labels
+
+
 def train():
     # load data
     train_data, train_labels, test_data, test_labels = load_data(DATA_FOLDER)
-    print(train_data.shape, train_labels.shape,
-          test_data.shape, test_labels.shape)
+    num_train = train_data.shape[0]
+    num_test = test_data.shape[0]
 
     # model
     model = make_model()
@@ -57,16 +76,44 @@ def train():
     # network settings
     outputs = model.forward(data)
     cross_entropy_loss = softmax_cross_entropy(labels, outputs)
-    optimizer = tf.train.AdamOptimizer(0.001)
-    train_step = optimizer.minimize(cross_entropy_loss)
-    print(train_step)
+    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy_loss)
+    _, batch_correct_num, batch_total_num = logits_accuracy(labels, outputs)
 
-    # train
+    # initialization
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
 
-    # test
+    for i in range(EPOCH):
+        print('-' * 20, '%d epoch' % (i + 1), '-' * 20)
 
-    # save model
-    pass
+        # train
+        train_data, train_labels = random_shuffle(train_data, train_labels)
+        for index_beg in range(0, num_train, TRAIN_BATCH_SIZE):
+            index_end = min(index_beg + TRAIN_BATCH_SIZE, num_train)
+            batch_data, batch_labels = load_batch(train_data, train_labels,
+                                                  index_beg, TRAIN_BATCH_SIZE)
+            loss, _ = sess.run([cross_entropy_loss, train_step],
+                               feed_dict={data: batch_data,
+                                          labels: batch_labels})
+            print('\rprogress = %0.2f%%, loss = %0.6f' % (
+                index_end / num_train * 100, loss), end='')
+        print()
+
+        # test
+        correct_num = 0
+        total_num = 0
+        for index_beg in range(0, num_test, TEST_BATCH_SIZE):
+            batch_data, batch_labels = load_batch(test_data, test_labels,
+                                                  index_beg, TEST_BATCH_SIZE)
+            c_num, b_num = sess.run([batch_correct_num, batch_total_num],
+                                    feed_dict={data: batch_data,
+                                               labels: batch_labels})
+            correct_num += c_num
+            total_num += b_num
+            print('\raccuracy = %0.4f' % (correct_num / total_num), end='')
+        print()
+
+        # save model
 
 
 if __name__ == '__main__':
