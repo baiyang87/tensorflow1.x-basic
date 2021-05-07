4815a78ba27fa22b8c6c9af5445855f0ebdec03f
baiyang <bby1220@163.com>
2021-05-07 23:01:12 +0800

    Change more information in `config`

--- test/test_plain_net.py
+++ test/test_plain_net.py
@@ -6,5 +6,7 @@ if __name__ == '__main__':
     inputs = tf.ones(shape=(7, 256, 256, 3))
     net = PlainNet()
     net.config['num_classes'] = 20
+    net.config['block_filters'] = [32, 64, 128]
+    net.config['block_conv_nums'] = [2, 2, 2]
     outputs = net.forward(inputs)
     print(outputs)
